5c71c94875bdb4de732653176f757bb4e27f7a63
Usman Nasution <usmannasty32@gmail.com>
2023-08-01 19:51:47 +0700

    some changes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,41 +1,46 @@
-import {Grid, Button} from '@mui/material';
+import {Grid, Button, Box} from '@mui/material';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+
 
 export default () => {
   return (
-    <>
+    <Box sx={{maxWidth:'500px', m:'auto', p:1}}>
       <Grid container alignItems="center">
         <Grid item xs={4}/>
         <Grid item xs={4}>
           <Button
             onClick={e => console.log(e.target)}
             fullWidth
+            size="large"
             children={<ArrowUpwardIcon/>}/>
         </Grid>
         <Grid item xs={4}/>
+      </Grid>
+      <Grid container sx={{my:1}}>
+        <Grid item xs={4}>
+          <Button
+            fullWidth
+            size="large"
+            children={<ArrowUpwardIcon/>}/>
+        </Grid>
+        <Grid item xs={4}/>
+        <Grid item xs={4}>
+          <Button
+            fullWidth
+            size="large"
+            children={<ArrowUpwardIcon/>}/>
+        </Grid>
       </Grid>
       <Grid container>
-        <Grid item xs={4}>
-          <Button
-            fullWidth
-            children={<ArrowUpwardIcon/>}/>
-        </Grid>
         <Grid item xs={4}/>
         <Grid item xs={4}>
           <Button
             fullWidth
-            children={<ArrowUpwardIcon/>}/>
-        </Grid>
-      </Grid>
-      <Grid container>
-        <Grid item xs={4}/>
-        <Grid item xs={4}>
-          <Button
-            fullWidth
+            size="large"
             children={<ArrowUpwardIcon/>}/>
         </Grid>
         <Grid item xs={4}/>
       </Grid>
-    </>
+    </Box>
   );
 };

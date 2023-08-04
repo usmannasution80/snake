581a4fbf27e56302424214664bc594af6b1f479e
Usman Nasution <usmannasty32@gmail.com>
2023-08-04 08:39:13 +0700

    Some bug fix

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import {Button, Box} from '@mui/material';
 
 export default () => {
@@ -21,6 +21,7 @@ export default () => {
     else if(direction === 'down')
       setActive(active >= sections.length - 1 ? 0 : active + 1);
   };
+
   window.web.selectOnClick = e => {
     switch (active) {
       case 0:

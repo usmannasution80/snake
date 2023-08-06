c7f72259516aa2e77e7326698876e1af1c86b9e3
Usman Nasution <usmannasty32@gmail.com>
2023-08-06 21:33:58 +0700

    Added snakebody, snakehead, and snaketail components

--- src/components/GameField.js
+++ src/components/GameField.js
@@ -2,8 +2,23 @@ import {Box} from '@mui/material';
 
 export default () => {
   return (
-    <>
-      GameField
-    </>
+    <svg
+      style={{width:'100%', background:'yellow'}}
+      viewBox="0 0 20 20">
+      <svg
+        width={1}
+        height={1}
+        viewBox="0 0 5 5"
+        fill="red"
+        x={0}
+        y={0}>
+          <rect
+            fill="red"
+            x={0}
+            y={0}
+            width={5}/>
+        </svg>
+      <svg width="300" height="100" style={{fill:'rgb(0,0,255)',strokeWidth:3,stroke:'rgb(0,0,0)'}} />
+    </svg>
   );
 };
---
+++ src/components/SnakeHead.js
@@ -0,0 +1,9 @@
+export default () => {
+  return (
+    <svg
+      width={1}
+      height={1}
+      viewBox="0 0 5 5">
+    </svg>
+  );
+};

012899df723dfda3bbc6e0fd1630edb4d9ae39ee
Usman Nasution <usmannasty32@gmail.com>
2023-08-08 15:35:06 +0700

    Updated SnakeBody

--- src/components/SnakeBody.js
+++ src/components/SnakeBody.js
@@ -1,6 +1,31 @@
-export default () => {
+export default (props) => {
+  const {x, y} = props;
   return (
-    <>
-    </>
+    <svg
+      width={1}
+      height={1}
+      x={x}
+      y={y}
+      viewBox="0 0 5 5">
+      <rect
+        x={0}
+        y={0}
+        width={5}
+        height={5}
+        fill="black"/>
+        <svg
+          width={4}
+          height={4}
+          x={0.5}
+          y={0.5}
+          viewBox="0 0 5 5">
+          <rect
+            x={0}
+            y={0}
+            width={5}
+            height={5}
+            fill="white"/>
+        </svg>
+    </svg>
   );
 };

5f8ddc3d88e0d6c7f82020a2a24c210d7c29ef97
Usman Nasution <usmannasty32@gmail.com>
2023-08-08 12:25:43 +0700

    Updated SnakeTail

--- src/components/SnakeTail.js
+++ src/components/SnakeTail.js
@@ -1,6 +1,42 @@
-export default () => {
+export default (props) => {
+  const {direction, x, y} = props;
+  const points = (() => {
+    let points;
+    switch(direction){
+      case 'up':
+        points = '0,5 5,5 2.5,0';
+        break;
+      case 'bottom':
+        points = '0,0 5,0 2.5,5';
+        break;
+      case 'left':
+        points = '0,0 0,5 5,2.5';
+        break;
+      default:
+        points = '5,0 5,5 0,2.5';
+    }
+    return points;
+  })();
   return (
-    <>
-    </>
+    <svg
+      width={1}
+      height={1}
+      viewBox="0 0 5 5"
+      x={x}
+      y={y}>
+      <polygon
+        points={points}
+        fill="black"/>
+      <svg
+        width={4}
+        height={4}
+        viewBox="0 0 5 5"
+        x={0.5}
+        y={0.5}>
+        <polygon
+          points={points}
+          fill="white"/>
+      </svg>
+    </svg>
   );
 };
